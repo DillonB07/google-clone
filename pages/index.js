6d3f95eeab8e01b36a426ae09ecb2b78deb7f97d
DillonB07 <dillonbarnes07@gmail.com>
2022-03-12 19:13:56 +0000

    Add Google's dot SVG

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
 import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -31,7 +33,14 @@ export default function Home() {
           </a>
 
           {/* Icon */}
-          <ViewGridIcon className="h-10 w-10 cursor-pointer rounded-full p-2 hover:bg-gray-100" />
+          {/* <ViewGridIcon className="h-10 w-10 cursor-pointer rounded-full p-2 hover:bg-gray-100" /> */}
+          <svg
+            className="gb_Se h-10 w-10 cursor-pointer rounded-full p-2 hover:bg-gray-100"
+            focusable="false"
+            viewBox="0 0 24 24"
+          >
+            <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+          </svg>
 
           <Avatar url="https://dillonb07.is-a.dev/static/images/DillonB07.png" />
         </div>
